d0c412737696c4d3a159465dc5683a9c73d7a906
Evgeni Egorov <zdezarus@gmail.com>
2023-05-30 15:56:17 +0500

    Update ActivityViewModelTest.kt

--- app/src/test/java/eu/maxkim/boredombuster/ActivityViewModelTest.kt
+++ app/src/test/java/eu/maxkim/boredombuster/ActivityViewModelTest.kt
@@ -14,4 +14,21 @@ class ActivityViewModelTest(){
     
     assert(viewModel.uiState.value is NewActivityUiState.Loading)
   }
+  
+  @Test
+  fun 'creating viewModel updates ui state to success after loading'(){
+       val viewModel = ActivityViewModel(
+      FakeGetRandomActivity(),
+      FakeIsActivitySaved(),
+      FakeSaveActivity(),
+      FakeDeleteActivity()
+      )
+       
+       val expectedUiState = NewActivitiUiState.Success(activity1, false)
+       
+       coroutineRule.testDispatcher.scheduler.runCurrent()
+       
+       val actualState = viewModel.uiState.value
+       assert(actualState, expectedUiState)
+  }
 }
