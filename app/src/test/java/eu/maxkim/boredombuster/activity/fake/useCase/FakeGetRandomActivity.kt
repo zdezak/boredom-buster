85c0278db718b44e1a5ed05caa1c6c66f434d7fd
Evgeni Egorov <zdezarus@gmail.com>
2023-05-30 16:24:44 +0500

    Update FakeGetRandomActivity.kt

--- app/src/test/java/eu/maxkim/boredombuster/activity/fake/useCase/FakeGetRandomActivity.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/fake/useCase/FakeGetRandomActivity.kt
@@ -1,7 +1,10 @@
 class FakeGetRandomActivity(private val isSuccessful: Boolean = true): GetRandomActivity{
+  //var activity: Activity? = null
+  var activity = activity1
   override suspend fun invoke(): Result<Activity>{
     return if(IsSuccessful){
-      Result.Successful(activity1)
+      //Result.Successful(activity?: activity1)
+      Result.Successful(activity)
     }else{
       Result.Error(RuntimeException("Boom..."))
     }
