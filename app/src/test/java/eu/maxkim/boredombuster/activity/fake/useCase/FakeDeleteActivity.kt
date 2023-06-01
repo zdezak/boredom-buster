2d745b4105ed0e13097660fa7a37140c132ce146
Evgeni Egorov <zdezarus@gmail.com>
2023-06-01 15:06:21 +0500

    Update FakeDeleteActivity.kt

--- app/src/test/java/eu/maxkim/boredombuster/activity/fake/useCase/FakeDeleteActivity.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/fake/useCase/FakeDeleteActivity.kt
@@ -2,7 +2,10 @@ import eu.maxkim.boredombuster.activity.model.Activity
 import eu.maxkim.boredombuster.activity.usecase.DeleteActivity
 
 class FakeDeleteActivity : DeleteActivity {
+    var wasCalled = false
+        private set
+
     override suspend fun invoke(activity: Activity) {
-        TODO("Not yet implemented")
+        wasCalled = true
     }
 }
