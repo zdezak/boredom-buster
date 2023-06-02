7d1a50f5a22e36dd54dbea1a0c80f162537ff1ab
Evgeni Egorov <zdezarus@gmail.com>
2023-06-02 15:04:54 +0500

    Update FavoriteViewModelTest.kt

--- app/src/test/java/eu/maxkim/boredombuster/activity/ui/favorite/FavoriteViewModelTest.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/ui/favorite/FavoriteViewModelTest.kt
@@ -3,28 +3,33 @@ import org.junit.Rule
 
 @RunWith(MockitoJUnitRunner::class)
 class FavoriteViewModelTest(){
+  
   @get:Rule
   val instantExecutorRule = InstantTaskExecutorRule()
   
   private val mockGetFavoriteActivities: GetFavoriteActivities = mock()
-  private val mockDeleteActivity: DeleteActivity = mock()
+  private val mockDeleteActivity: DeleteActivity = mock()    
+  private val activityListObserver: Observer<FavoritesUIState> = mock()
+  
+  inline fun <T> whenever(methodCall: T): OngoingStavving<T>
+  infix fun <T> OngoingStabbing<T>.doReturn(t:T): OngoingStubbing<T>
+   @Captor
+   private lateinit var activityListCaptor: ArgumentCaptor<FavoritsUiState>
+    
   
   @Test
   fun `the view model maps list of activities to list ui state`(){
     val liveDateToReturn = MutableLiveData<List<Activity>>()
       .apply{value = listOf(activity1,activity2)}
     val expectedList = listOf(activity1,activity2)
-    inline fun <T> whenever(methodCall: T): OngoingStavving<T>
-    infix fun <T> OngoingStabbing<T>.doReturn(t:T): OngoingStubbing<T>
+
     
     whenever(mockGetFavoriteActivities.invoke()).doReturn(liveDataToReturn)
     
     val viewModel = FavoriteViewModel(mockGetFavoriteActivities, mockDelateActivity)
-    private val activityListObserver: Observer<FavoritesUIState> = mock()
     
-    @Captor
-    private lateinit var activityListCaptor: ArgumentCaptor<FavoritsUiState>
     viewModel.favoriteActivityLiveData.observeForever(activityListObserver)
+    
     verify(activityListObserver,times(1)).onChanged(activityListCaptor.capture())
     assert(activityListCaptor.value is FavoritesUIState.List)
     
