9155c272a39e15c6ac878dbe954d22fe731389f2
Evgeni Egorov <zdezarus@gmail.com>
2023-06-06 16:24:38 +0500

    Update ActivityLocalDataSourceImplTest.kt

--- app/src/androidTest/java/eu/maxkim/boredombuster/ActivityLocalDataSourceImplTest.kt
+++ app/src/androidTest/java/eu/maxkim/boredombuster/ActivityLocalDataSourceImplTest.kt
@@ -2,6 +2,14 @@ class ActivityLocalDataSourceImplTest{
   private lateinit var activityDao: ActivityDao
   private lateinit var database: AppDatabase
   
+  @GetRule
+  val instantExecutorRule = InstantTaskExecutorRule()
+  
+  private val activityListObserver: Observer<List<Activity>> = mock()
+ 
+  @Captor
+  private lateinit var activityListCaptor: ArgumentCaptor<List<Activity>>
+  
   @Before
   fun createDb(){
     val context = ApplicationProvider.getApplicationContext<Context>()
@@ -32,5 +40,19 @@ class ActivityLocalDataSourceImplTest{
     activityLocalDataSource.deleteActivity(androidActivity1)
     
     assert(!activityLocalDataSource.isActivitySaved(androidActivity1.key))
+  }
+  
+  @Test
+  fun canSaveActivityToTheDbAndObserveTheLiveData() = runTest{
+    val activityLocalDataSource = ActivityLocalDataSourceImpl(activityDao)
+    val expectedList = listOf(androidActivity1, androidActivity2)
+    
+    activityLocalDataSource.getActivityListLiveData()
+    .observeForever(activityListObserver)
+    activityLocalDataSource.saveActivity(androidActivity1)
+    activityLocalDataSource.saveActivity(androidActivity2)
+    
+    verify(activityListObserver, times(3)).onChanged(activityListCaptor.capture())
+    assertEquals(actiityListCaptor.value, expectedList)
   }
 }
